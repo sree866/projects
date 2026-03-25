6b049d784f6213aecde3b8379fbb17dfe95f745e
sree <abcd@gmail.com>
2026-03-25 13:04:18 +0530

    file4

---
+++ Dhana.java
@@ -0,0 +1,10 @@
+public class Dhana {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i <= 5) {
+            System.out.println("Number: " + i);
+            i++;
+        }
+        System.out.println("Done");
+    }
+}
